5b0e51e5352a5f7a55ef281e58648ed980629f0a
“Alan <alan.hillgemann@anz.com>
2021-05-17 14:56:35 +1000

    feat: implement logout feature

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
@@ -24,6 +24,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http
+                .logout()
+                .logoutSuccessUrl("/login");
         http.authorizeRequests()
                 .antMatchers("/signup", "/css/**", "/js/**").permitAll()
                 .anyRequest().authenticated();
